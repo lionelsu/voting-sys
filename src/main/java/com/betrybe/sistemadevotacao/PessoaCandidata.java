471296661ee4211b81322fffdd93053b88ba8e25
lionelsu <lionel_suicune@hotmail.com>
2023-12-03 12:49:45 -0400

    feat: concrete class PessoaCandidata

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,8 +1,37 @@
 package com.betrybe.sistemadevotacao;
 
 /**
- * Classe abstrata que representa uma pessoa.
+ * Classe concreta que representa uma pessoa candidata.
  */
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
+  protected int numero;
+  protected int votos;
 
+  /**
+   * Construtor da classe PessoaCandidata.
+   *
+   * @param nome   Nome do candidato.
+   * @param numero Número indicador para o voto.
+   */
+  public PessoaCandidata(String nome, int numero) {
+    setNome(nome);
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void setNumero(int votos) {
+    this.votos = votos;
+  }
+
+  public void receberVoto() {
+    this.votos += 1;
+  }
 }
